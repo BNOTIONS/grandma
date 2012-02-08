3d1b45ee16bbae64e25a9e3a514f015b066ee4f4
Muntasir Mohiuddin <muntasir.mohiuddin@gmail.com>
2012-02-08 14:10:29 -0500

    adding database configuration variable

--- init.php
+++ init.php
@@ -1,2 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+// Configure the database module to use the proper database configuration based on environment
+Database::$default = Kohana::$environment;
+
